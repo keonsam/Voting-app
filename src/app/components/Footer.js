aec91dcd8f834ebb0bd91209ae966c6c70c25939
keon <keonsamuel95@gmail.com>
2017-12-04 22:27:25 -0400

    routing updates

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,14 +1,9 @@
 import React from "react";
 
-export class Footer extends React.Component{
-  constructor(props){
-    super(props);
-  }
-  render() {
-    return(
+export const Footer = () => {
+  return(
       <div className="text-center footer">
         <p id="footer">Created by<a href="https://github.com/keonsam" id="name"> Keon</a> with <span id="icon"><i className="fa fa-heart" aria-hidden="true"></i></span></p>
     </div>
     );
-  }
 }
--- src/app/index.js
+++ src/app/index.js
@@ -1,33 +1,9 @@
 import React from "react";
 import { render } from "react-dom";
-import { Route, NavLink, HashRouter } from 'react-router-dom';
+//import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
 import '../css/main.scss';
-import { Header } from "./components/Header";
-import { Banner } from "./components/Banner";
-import { Body } from "./components/Body";
-import { Footer } from "./components/Footer";
-import { Signup } from "./components/Signup";
-import { Login } from "./components/Login";
 
-class App extends React.Component{
-  constructor(props){
-    super(props);
-  }
-  render() {
-    return(
-      <HashRouter>
-      <div className="container-fluid p-0">
-        <Header/>
-        <Banner/>
-        <Route path="/" component={Body}/>
-        <Route path="/Signup" component={Signup}/>
-        <Route path="/Login" component={Login}/>
-        <Footer/>
-      </div>
-      </HashRouter>
-    );
-  }
-}
+import { App } from "./components/App";
 
 render(<App/>, document.getElementById('app'));
